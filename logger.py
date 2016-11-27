254030470715b54ee8a0c598b9804c3e85e16669
user2684 <user2684@users.noreply.github.com>
2016-11-27 16:14:14 +0100

    #51 Capability to rotate the application's logs

--- logger.py
+++ logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import logging 
+import logging.handlers
 
 import config
 conf = config.get_config()
@@ -26,7 +27,7 @@ def get_console_logger(logger_name):
 
 # return a file logger
 def get_file_logger(logger_name):
-	file = logging.FileHandler(conf["constants"]["log_dir"]+"/"+conf["logging"][logger_name]["filename"])
+	file = logging.handlers.RotatingFileHandler(conf["constants"]["log_dir"]+"/"+conf["logging"][logger_name]["filename"],maxBytes=conf["logging"]["rotate_size_mb"]*1024*1024, backupCount=conf["logging"]["rotate_count"])
 	file.setLevel(get_level(conf["logging"][logger_name]["level"]))
 	file.setFormatter(conf["constants"]["log_formatter"])
 	return file
