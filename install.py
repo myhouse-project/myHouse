176a58a71067e3613d199b188629a0b801687168
username <user@domain.com>
2016-12-17 18:48:41 +0100

    #92 A number of python modules are missing when running install.py

--- install.py
+++ install.py
@@ -1,88 +1,96 @@
 #!/usr/bin/python
 import sys
 import os
-
-import utils
-import logger
-import config
-log = logger.get_logger(__name__)
-conf = config.get_config()
+import subprocess
 
 # variables
-filename = conf['constants']['service_location'].split('/')[-1]
+base_dir = os.path.abspath(os.path.dirname(__file__))
+service_template = base_dir+"/template_service.sh"
+service_location = '/etc/init.d/myhouse'
+filename = service_location.split('/')[-1]
+debug = False
+
+# run a command and return the output
+def run_command(command):
+        if debug: print "Executing "+command
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = ''
+        for line in process.stdout.readlines():
+                output = output+line
+	if debug: print output.rstrip()
 
 # install all the dependencies
 def install_deps():
-	log.info("Preparing dependencies...")
-	log.info("Installing redis...")
-	log.debug(utils.run_command("apt-get install redis"))
-	log.info("Installing flask...")
-	log.debug(utils.run_command("apt-get install python-flask"))
-        log.info("Installing python-redis...")
-        log.debug(utils.run_command("apt-get install python-redis"))
-        log.info("Installing python-numphy...")
-        log.debug(utils.run_command("apt-get install python-numpy"))
-        log.info("Installing python-rpi.gpio...")
-        log.debug(utils.run_command("apt-get install python-rpi.gpio"))
-        log.info("Installing python-apscheduler...")
-        log.debug(utils.run_command("pip install APScheduler"))
-        log.info("Installing python-slackclient...")
-        log.debug(utils.run_command("pip install slackclient"))
-        log.info("Installing python-simplejson...")
-        log.debug(utils.run_command("pip install simplejson"))
-        log.info("Installing python-levenshtein...")
-        log.debug(utils.run_command("pip install python-Levenshtein"))
-        log.info("Installing python-fuzzywuzzy...")
-        log.debug(utils.run_command("pip install fuzzywuzzy"))
-        log.info("Installing python-pyicloud...")
-        log.debug(utils.run_command("pip install pyicloud"))
-        log.info("Installing python-motionless...")
-        log.debug(utils.run_command("pip install motionless"))
-        log.info("Installing python-flask-compress...")
-        log.debug(utils.run_command("pip install flask-compress"))
-        log.info("Installing python-jsonschema...")
-        log.debug(utils.run_command("pip install jsonschema"))
+	print "Preparing dependencies..."
+	print "Installing redis..."
+	run_command("apt-get install redis-server")
+	print "Installing flask..."
+	run_command("apt-get install python-flask")
+        print "Installing python-redis..."
+        run_command("apt-get install python-redis")
+        print "Installing python-numphy..."
+        run_command("apt-get install python-numpy")
+        print "Installing python-rpi.gpio..."
+        run_command("apt-get install python-rpi.gpio")
+        print "Installing python-apscheduler..."
+        run_command("pip install APScheduler")
+        print "Installing python-slackclient..."
+        run_command("pip install slackclient")
+        print "Installing python-simplejson..."
+        run_command("pip install simplejson")
+        print "Installing python-levenshtein..."
+        run_command("pip install python-Levenshtein")
+        print "Installing python-fuzzywuzzy..."
+        run_command("pip install fuzzywuzzy")
+        print "Installing python-pyicloud..."
+        run_command("pip install pyicloud")
+        print "Installing python-motionless..."
+        run_command("pip install motionless")
+        print "Installing python-flask-compress..."
+        run_command("pip install flask-compress")
+        print "Installing python-jsonschema..."
+        run_command("pip install jsonschema")
 
 # installation routine
 def install():
 	install_deps()
-	log.info("Installing the program...")
+	print "Installing the program..."
 	# prepare the service template
-	with open(conf['constants']['service_template'], 'r') as file:
+	with open(service_template, 'r') as file:
 		template = file.read()
-	template = template.replace("#base_dir#",conf['constants']['base_dir'])
+	template = template.replace("#base_dir#",base_dir)
 	# write the service script
-	log.info("Creating the service script...")
-	with open(conf['constants']['service_location'],'w') as file:
+	print "Creating the service script..."
+	with open(service_location,'w') as file:
 		file.write(template)
 	file.close()
 	# make it executable
-	utils.run_command("chmod 755 "+conf['constants']['service_location'])
+	run_command("chmod 755 "+service_location)
 	# add it as a service
-	log.info("Adding it as a service...")
-	log.debug(utils.run_command("update-rc.d "+filename+" defaults"))
+	print "Adding it as a service..."
+	run_command("update-rc.d "+filename+" defaults")
 	# start the service
-	log.info("Starting the service...")
-	log.debug(utils.run_command("service "+filename+" start"))
-	log.info("Done")
+	print "Starting the service..."
+	run_command("service "+filename+" start")
+	print "Done"
 
 # uninstall routine
 def uninstall():
-	log.info("Uninstalling the program...")
+	print "Uninstalling the program..."
         # stop the service
-	log.info("Stopping the service...")
-        log.debug(utils.run_command("service "+filename+" stop"))
+	print "Stopping the service..."
+        run_command("service "+filename+" stop")
 	# remove the script
-	log.info("Uninstalling the service...")
-	log.debug(utils.run_command("rm -f "+conf['constants']['service_location']))
+	print "Uninstalling the service..."
+	run_command("rm -f "+service_location)
 	# disable the service
-	log.debug(utils.run_command("update-rc.d -f "+filename+" remove"))
+	run_command("update-rc.d -f "+filename+" remove")
 
 # ensure it is run as root
 if os.geteuid() != 0:
         exit("ERROR: You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
 # run the installation
-log.info("Welcome to myHouse v"+conf["constants"]["version_string"])
+print "Welcome to myHouse"
 if len(sys.argv) == 2 and sys.argv[1] == "-u": uninstall()
 else: install()
 
