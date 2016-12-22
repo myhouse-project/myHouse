712954679885f5e0d3f9b3f37c54fd67b51ed5c0
username <user@domain.com>
2016-12-22 19:06:56 +0100

    First implementation of plugin_fdsnws

--- plugin_fdsnws.py
+++ plugin_fdsnws.py
@@ -11,12 +11,13 @@ conf = config.get_config()
 
 plugin_conf = conf['plugins']['fdsnws']
 limit = 10000
+query = "format=text&limit="+str(limit)+"&orderby=time-asc"
 
 # poll the sensor
 def poll(sensor):
 	# query the service
-	url = "http://"+sensor["plugin"]["domain"]+"/fdsnws/event/1/query?format=text&limit="+limit+"&"+sensor["plugin"]["query"]
-	data = utils.web_get(filename)
+	url = "http://"+sensor["plugin"]["domain"]+"/fdsnws/event/1/query?"+query+"&"+str(sensor["plugin"]["query"])
+	data = utils.web_get(url)
 	return json.dumps(data)
 
 # parse the data
@@ -26,36 +27,34 @@ def parse(sensor,data):
 	# load the file
 	data = json.loads(data)
 	# for each line
-	for line in data:
+	for line in data.split('\n'):
+                #EventID|Time|Latitude|Longitude|Depth/Km|Author|Catalog|Contributor|ContributorID|MagType|Magnitude|MagAuthor|EventLocationName
+                #    0    1      2          3       4       5     6           7            8           9     10         11           12
+		if line.startswith('#'): continue
 		measure = {}
+		# split the entries
 		entry = line.split('|')
-		#EventID|Time|Latitude|Longitude|Depth/Km|Author|Catalog|Contributor|ContributorID|MagType|Magnitude|MagAuthor|EventLocationName
-		#    0    1      2          3       4       5     6           7            8           9     10         11           12
+		if len(entry) != 13: continue
+		# set the timestamp to the event's date
 		date_format = "%Y-%m-%dT%H:%M:%S.%f"
 		date = datetime.datetime.strptime(entry[1],date_format)
-		# if a filter is defined, ignore the line if the filter is not found
-		if "filter" in sensor["plugin"] and entry[sensor["plugin"]["filter_position"+1]] != sensor["plugin"]["filter"]: continue
-		# if a prefix is defined, filter based on it
-		if "prefix" in sensor["plugin"] and not entry[sensor["plugin"]["value_position"+1]].startswith(sensor['plugin']['prefix']): continue
-		# generate the timestamp
-		if "date_position" in sensor["plugin"]:
-			date = datetime.datetime.strptime(entry[sensor["plugin"]["date_position"+1]],sensor["plugin"]["date_format"])
-			measure["timestamp"] = utils.timezone(utils.timezone(int(time.mktime(date.timetuple()))))
-		else: measure["timestamp"] = utils.now()
-		# set the key as the sensor_id
-		measure["key"] = sensor["sensor_id"]
-		# strip out the measure from the value
-		value = entry[sensor["plugin"]["value_position"+1]]
-		# if a measure prefix was defined, remove it
-		if "prefix" in sensor["plugin"]: value.replace(sensor['plugin']['prefix'],"")
-		# set the value
-		measure["value"] = utils.normalize(value,conf["constants"]["formats"][sensor["format"]]["formatter"])
+		measure["timestamp"] = utils.timezone(utils.timezone(int(time.mktime(date.timetuple()))))
+		# prepare the position value
+		position = {}
+		position["latitude"] = float(entry[2])
+		position["longitude"] = float(entry[3])
+		position["label"] = str(entry[10])
+		date_string = utils.timestamp2date(int(measure["timestamp"]))
+		position["text"] = str("<p><b>"+entry[12]+":</b></p><p>Magnitude: "+entry[10]+"</p><p>Date: "+date_string+"</p><p>Depth: "+entry[4]+" km</p>")
+		# prepare the measure
+		measure["key"] = sensor["sensor_id"]+":day:avg"
+		measure["value"] = json.dumps(position)
+		# add the event to the measures
 		measures.append(measure)
         return measures
 
 # return the cache schema
 def cache_schema(sensor):
 	# cache the entire file
-	filename = sensor["plugin"]["csv_file"] if "csv_file" in sensor["plugin"] else plugin_conf['csv_file']
-	return filename
+	return sensor["plugin"]["domain"]
 
